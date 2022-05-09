814424e6cbb6f1fd01468364ae2035541ebae1f0
Rehtse Studio <efelicies@letsgomango.com>
2022-05-09 13:42:52 -0400

    Playeron the scene with is scripts.  Player can move and shoot

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private float _speed = 7.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveUpLaser();
+        DestroyLaser();
+    }
+
+    void MoveUpLaser()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+
+    void DestroyLaser()
+    {
+        if(transform.position.y > 8f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _speed;
+    private Vector3 _movement;
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    //Projectiles Variable Section
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private GameObject _lasersContainer;
+
+    //Cooldown Variable Section
+    private float _firingRate = 0.2f;
+    private float _canIFire = -1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootingLaser();
+        PlayerMovement();
+    }
+
+    void ShootingLaser()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canIFire)
+        {
+            _canIFire = Time.time + _firingRate;
+            Instantiate(_laserPrefab, transform.position + new Vector3(0,0.8f,0), Quaternion.identity,_lasersContainer.transform);
+        }
+    }
+
+    void PlayerMovement()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+
+        _movement = new Vector3(_horizontalInput, _verticalInput, 0);
+
+        transform.Translate(_movement * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+}
